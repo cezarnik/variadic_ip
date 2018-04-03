9cee828f6d58f4da90196e55c1cb50a4efe49aa3
cezarnik <cezarnik2@yandex.ru>
2018-04-03 09:14:37 +0300

    simplified code. deleted variadic version of 'contains'

--- filter.h
+++ filter.h
@@ -5,29 +5,17 @@
 #include <algorithm>
 #include <tuple>
 #include <string>
-template<class T>
-std::vector<int> contains(int pos,T pattern){
-    	std::vector<int> res=std::vector<int>();
+
+typedef std::tuple<int,int,int,int> IP;
+std::vector<int> contains(IP ip,int element){
+	std::vector<int> res;
+	std::vector<int> els(4);
+	std::tie(els[0],els[1],els[2],els[3])=ip;
+	for(int i=0; i<4; ++i){
+		if (element==els[i])
+			res.push_back(i+1);
+	}
 	return res;
-}
-
-//return vector of positions of matches
-template<class T,typename... Args>
-std::vector<int> contains(int pos,T pattern,T s,Args... args){
-    std::vector<int> res=contains(pos+1,pattern,args...);
-    if (s==pattern){
-        res.push_back(pos);
-    }
-    return res;
-}
-
-//convert string to int
-int convert(std::string & s){
-    int res=0;
-    for(int i=0; i<(int)s.size(); ++i){
-        res=res*10+s[i]-'0';
-    }
-    return res;
 }
 
 //split the string using delimeter
@@ -46,17 +34,7 @@ std::vector<std::string> split(const std::string &str, char d){
 }
 
 
-template<typename T>
-void print(const T el){
-    std::cout<<el<<std::endl;
-}
-
-template<typename T,typename... Args>
-void print(const T el,Args... args){
-    std::cout<<el<<".";
-    print(args...);
-}
-void print(std::tuple<int,int,int,int> tup){
-    print(std::get<0>(tup),std::get<1>(tup),std::get<2>(tup),std::get<3>(tup));
+void print(IP tup){
+std::cout<<std::get<0>(tup)<<"."<<std::get<1>(tup)<<"."<<std::get<2>(tup)<<"."<<std::get<3>(tup)<<std::endl;
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -1,38 +1,37 @@
 #include "filter.h"
-std::vector<std::tuple<int,int,int,int>> IPs;
-
+#include <functional>
+std::vector<IP> IPs;
 
 int main(){
     std::string s;
     while(getline(std::cin,s)){
         auto strs=split(s,'\t');
         auto one=split(strs[0],'.');
-        auto tup=std::make_tuple(convert(one[0]),convert(one[1]),convert(one[2]),convert(one[3]));
+        auto tup=std::make_tuple(std::stoi(one[0]),std::stoi(one[1]),std::stoi(one[2]),std::stoi(one[3]));
         IPs.push_back(tup);
     }
-    std::sort(IPs.begin(),IPs.end());
-    std::reverse(IPs.begin(),IPs.end());
-    for(auto it:IPs){
-        print(it);
+    std::sort(IPs.begin(),IPs.end(),std::greater<IP>());
+    for(auto ip:IPs){
+        print(ip);
     }
-    for(int i=0; i<(int)IPs.size(); ++i){
-        std::vector<int> good=contains(1,1,std::get<0>(IPs[i]));
+    for(auto ip:IPs){
+        auto good=contains(ip,1);
         if (!good.empty() && good[0]==1){
-            print(IPs[i]);
+            print(ip);
         }
     }
-    for(int i=0; i<(int)IPs.size(); ++i){
-        std::vector<int> good_46=contains(1,46,std::get<0>(IPs[i]),std::get<1>(IPs[i]));
-        std::vector<int> good_70=contains(1,70,std::get<0>(IPs[i]),std::get<1>(IPs[i]));
+    for(auto ip:IPs){
+        auto good_46=contains(ip,46);
+        auto good_70=contains(ip,70);
 	auto it_46=find(good_46.begin(),good_46.end(),1);
         auto it_70=find(good_70.begin(),good_70.end(),2);
         if (it_46!=good_46.end() && it_70!=good_70.end())
-            print(IPs[i]);
+            print(ip);
     }
-    for(int i=0; i<(int)IPs.size(); ++i){
-        auto good=contains(1,46,std::get<0>(IPs[i]),std::get<1>(IPs[i]),std::get<2>(IPs[i]),std::get<3>(IPs[i]));
+    for(auto ip:IPs){
+        auto good=contains(ip,46);
         if (!good.empty())
-            print(IPs[i]);
+            print(ip);
     }
 }
 
